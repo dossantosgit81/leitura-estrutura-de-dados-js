b1267e9aa8bc074b7369b167aa1c1609a1e5c669
Rafael Mendes dos Santos <dossantosgit81@gmail.com>
2022-07-25 06:40:02 -0300

    Usa classe stack e organiza melhor o código

--- pilhas/stack-array/StackArray.js
+++ pilhas/stack-array/StackArray.js
@@ -1,4 +1,4 @@
-class StackArray {
+export class StackArray {
 
     constructor() {
         this.items = [];
---
+++ pilhas/stack-array/index.js
@@ -0,0 +1,5 @@
+//Para esse tipo de arquivo, é necessário que a extensão .js esteja declarada.
+import {StackArray} from "./StackArray.js";
+
+const stack = new StackArray();
+console.log(stack.isEmpty())
