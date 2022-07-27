fabb07c10e3215eca3f56d79102b6fa5ac0251af
Rafael Mendes dos Santos <dossantosgit81@gmail.com>
2022-07-27 06:37:27 -0300

    cria clear isEmpty toString and peek and pop

--- pilhas/stack-object/StackObject.js
+++ pilhas/stack-object/StackObject.js
@@ -6,7 +6,54 @@ export default class StackObject {
     }
 
     push(element){
+        console.log(this);
+        console.log(this.isEmpty());
         this.items[this.count] = element;
         this.count++;
     }
+
+    size(){
+        return this.count;
+    }
+
+    isEmpty(){
+        return this.count == 0;
+    }
+
+    pop() {
+        console.log(this);
+        if(this.isEmpty) {
+            return undefined;
+        }
+
+        this.count--;
+        const result = this.items[this.count];
+        delete this.items[this.count];
+        return result;
+    }
+
+    peek(){
+        if(this.isEmpty){
+            return undefined;
+        }
+        return this.items[this.count-1];
+    }
+
+    clear(){
+        /**
+         * while(!this.isEmpty){
+         *  this.pop;
+         * }
+         * 
+         */
+        this.items = {};
+        this.count = 0;
+    }
+
+    toString(){
+        if(this.isEmpty){
+            return '';
+        }
+    }
+
 }
